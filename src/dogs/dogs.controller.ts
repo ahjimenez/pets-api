c72aa5bd30a7fa155e8b403470241b607378aa81
Angel H. Jimenez <ahjimenez@hotmail.com>
2025-06-13 04:04:39 -0600

    Implement dogs module

---
+++ src/dogs/dogs.controller.ts
@@ -0,0 +1,30 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { DogsService } from './dogs.service';
+import { Dog } from './types/dog.type';
+import { DogDto, UpdateDogDto } from './types/dog.dto';
+
+@Controller('dogs')
+export class DogsController {
+
+    constructor(private readonly dogsService: DogsService) {}
+
+    @Get()
+    indexedDB() {
+        return this.dogsService.findAll();
+    }
+
+    @Post()
+    create(@Body() creatadogDto: DogDto) {
+        return this.dogsService.create(creatadogDto);
+    }
+
+     @Patch(':id')
+    update(@Param('id') id: string, @Body() updateDogDto: UpdateDogDto) {
+        return this.dogsService.update(+id, updateDogDto);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.dogsService.delete(+id);
+    }
+    }   
